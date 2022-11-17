2caeb34169def0f75174c9068f7bfe31caa5a9be
Jonathan Mallia <jonathan.mallia@genesis.com.mt>
2022-11-17 15:42:33 +0100

    first commit

--- Generate_Requirements_txt.bat
+++ Generate_Requirements_txt.bat
@@ -1,6 +1,6 @@
 pip install pipreqs
+python -m pipreqs.pipreqs "C:\Users\JonathanM\OneDrive\OneDrive - Genesis Global Limited\Desktop\LiveChat Chats EDA\Scripts" --force
 
-python -m pipreqs.pipreqs "C:\Users\JonathanM\OneDrive\OneDrive - Genesis Global Limited\Desktop\VIP Distance\Scripts" --force
 
 
 
